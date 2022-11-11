e4f7c5af6d0bf6f31f8f57804d78755366c6c7f2
Mansour Yusupov <mansur.yusupov.05@icloud.com>
2022-11-11 17:15:55 +0500

    2 home-work

---
+++ 2 home task/main.js
@@ -0,0 +1,77 @@
+//q1
+// let n=120;
+// let h=Math.floor(n/60);
+// let m=n%60;
+// console.log(h%24,m);
+
+//q2
+// let n=3602;
+// let h=Math.floor(n/3600);
+// let m=Math.floor(n%24/24);
+// let s=n%24;
+// console.log(`${h}:0${m/10%10}:${s/10%10}`);
+
+//q3
+// let n=2;
+// let a=10*n;
+// let b=200*n;
+// console.log(a+b/100,b%100);
+
+//q4
+// let h1=1
+// let m1=1
+// let s1=1
+// let h2=2
+// let m2=2
+// let s2=2
+// let sum1=h1*3600+m1*60+s1;
+// let sum2=h2*3600+m2*60+s2;
+// console.log(sum2-sum1);
+
+
+//q5
+// let n=2002;
+// let a=Math.floor(n/1000);
+// let b=Math.floor(n%1000/100);
+// let c=Math.floor(n%100/10);
+// let d=Math.floor(n%10);
+// if(a==d && b==c){
+//     console.log(`YES`);
+// }
+// else{
+//     console.log(`NO`);
+// }
+
+//q6
+// let a=1;
+// let b=2;
+// let c=3;
+// let cnt=0;
+// if(a>0){
+//     cnt++;
+// }
+// if(b>0){
+//     cnt++;
+// }
+// if(c>0){
+//     cnt++;
+// }
+// console.log(cnt);
+
+//q7
+// let a=8;
+// for(let i=1; i<=a; i++){
+//     console.log(i);
+// }
+// for(let i=a; i>=1; i--){
+//     console.log(i);
+// }
+
+//q8
+// let a=2;
+// let b=5;
+// let cnt=0;
+// for(let i=a; i<=b; i++){
+//     cnt+=i**2;
+// }
+// console.log(cnt);
