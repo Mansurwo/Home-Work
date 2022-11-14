19e244eb7d84ae9adba123c90b98aaffd14f6a22
Mansour Yusupov <mansur.yusupov.05@icloud.com>
2022-11-14 19:51:43 +0500

    New Home Work

---
+++ 2 home task/Readme.md
@@ -0,0 +1,23 @@
+# Home Practice
+#  Task 1
+### In first example we must minutes turn to hour
+# Task 2
+### In second example we must minutes turn to hour and second
+# Task 3
+### In third example we must somoni turn to diram and sum diram and somoni
+### at second time multiply sum and count turn to somoni
+# Task 4
+### In fourth example turn 3 1 to second and turn 3 2 to second 
+### at subtrack first second to second
+# Task 5
+### At fifth example we trust check the numbers is simetric
+### if number is simetric console.log(YES)
+### else number not simetric console.log(No)
+# Task 6
+### At sixth example we trust search positive number if we found positive number 
+### cnt++
+# Task 7
+### At seventh example we excute for at 1 to 6
+# Task 8
+### At eight example we excute for and find square 
+### Thanks For listening
---
+++ 4 home task/main.js
@@ -0,0 +1,88 @@
+// task1
+// let text = prompt()
+// let text2 = ""
+// for(let i=0; i<text.length; i++){
+//     text2+=text[i]+text[i]
+// }
+// console.log(text2)
+
+
+// task2
+// let text = prompt()
+// let cnt = 0
+// for(let i=0; i<text.length; i++){
+//     text[i]=="p"&&text[i+1]=="o"&&text[i+2]=="t"&&text[i+3]=="a"&&text[i+4]=="t"&&text[i+5]=="o"?cnt++:cnt;
+// }
+// console.log(cnt)
+
+
+// task3
+// let text = prompt()
+// let text2 = prompt()
+// let cnt = 0
+// for(let i=0; i<text2.length; i++){
+//     text2[i]==text?cnt++:cnt
+// }
+// console.log(cnt)
+
+
+// task 4
+// let text = prompt()
+// let cnt = 0
+// for(let i=0; i<text.length; i++){
+//     text[i]=="a"||text[i]=="e"||text[i]=="i"||text[i]=="o"||text[i]=="u"?cnt++:cnt
+// }
+// console.log(cnt)
+
+
+//task 5
+// let text = prompt()
+// text.includes("bomb")?console.log("Duck!!"):console.log("Relax")
+
+
+// task 6
+// let text = prompt()
+// let result = text.replace(/[a o u i e]/gi, "")
+// console.log(result) 
+
+
+//task7
+// let text=prompt()
+// let text2=text.split()
+// for(i=0;i<text.length;i++){
+//     if(text2[i]=="a" || text2[i]=="o" || text2[i]=="u" || text2[i]=="i" || text2[i]=="y"){
+//         console.log(i)
+//     }
+// }
+
+
+//task 8
+// let text = prompt()
+// let text2 = +prompt()
+// if(text2==1){
+//     console.log(`Hello ${text.slice(0,1).toUpperCase()+text.slice(1).toLowerCase()}`)
+// }
+// if(text2==0){
+//     console.log(`Bye ${text.slice(0,1).toUpperCase()+text.slice(1).toLowerCase()}`)
+// }
+
+
+// task 9
+// let text = prompt("Country")
+// let text2 = +prompt("Number")
+// let result = (text2*100)/148.9e+6
+// console.log(`${text} is ${result.toFixed(2)}% of the total world's landmass`)
+
+
+//task10
+// let text = prompt()
+// let result = text.split(" ")
+// let result2 = result.reverse()
+// console.log(result2.join(" "))
+
+
+// task11
+// let text = prompt()
+// let result = text.split("")
+// let result2 = result.reverse()
+// console.log(result2.join("")+ "-"+ text)
