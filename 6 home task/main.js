330f668ad71ba3d338244becc3c32fea6048e9f9
Mansour Yusupov <mansur.yusupov.05@icloud.com>
2022-11-17 18:08:07 +0500

    6 home task

---
+++ 6 home task/main.js
@@ -0,0 +1,106 @@
+// task 1 calculator
+// function calculator(a, b, c){
+//     b == "+"? console.log(a+c):b == "*"?console.log(a*c): b == "/"?console.log(a/c):console.log("Nestay")
+// }
+// console.log(calculator(2,"+",6));
+
+
+// task 1 calculator
+    // function calculator(a, b, c){
+    //     if(b=="+"){
+    //         return a+c
+    //     }
+    //     if(b=="-"){
+    //         return a-c
+    //     }
+    //     if(b=="*"){
+    //         return a*c
+    //     }
+    //     if(b=="/"){
+    //         return a/c
+    //     }
+    // }
+    // console.log(calculator(2,"+",6));
+
+
+// task 2
+// function numarg(...a){
+//     return a.length
+// }
+// console.log(numarg(1,2,3,4,5,4))
+
+
+// task 3 keyboard mistakes
+// function keyboardmis(name){
+//     return name.replace("4","A").replace("5","S").replace("1","I").replace("0","O")
+// }
+// console.log(keyboardmis("MUB45H1R"))
+
+
+// task 4 less than
+// function lessthan(a,b){
+//     let res = a + b
+//     if(res<100){
+//         return true 
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(lessthan(22,37))
+
+
+// task 4 ternary
+// function lessthan(a,b){
+//     let res = a + b
+//     res < 100 ? console.log(true):console.log(false) 
+// }
+// console.log(lessthan(22,37))
+
+
+// task 5 buggy code
+// function buggy(a){
+//     return a * a * a
+// }
+// console.log(buggy(3))
+
+
+// task 6 area of triangle
+// function area(a,b){
+//     return (a*b)/2
+// }
+// console.log(area(3,2))
+
+
+// task 7 equal number
+// function numequal(a,b){
+//     if(a === b){
+//        return true
+//     }
+//     else{
+//        return false
+//     }
+// }
+// console.log(numequal(2,"2"));
+
+
+// task 8 world char world
+// function add(a,b){
+//     return b.replace(" ",a)
+// }
+// console.log(add("R","Mansu  Yusupov"))
+
+
+// task 9 max min
+// function maxmin(num){
+//     let res = num.split(" ")
+//     return Math.max(...res)+ "  " +Math.min(...res)
+// }
+// console.log(maxmin("1 2 3 4 5"));
+
+
+// task 10 reverse
+// function rever(a){
+//     return a.toUpperCase().split('').reverse().join('')
+//   }
+//   console.log(rever("hello"))
